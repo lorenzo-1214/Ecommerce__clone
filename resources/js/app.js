75cc65e27f48a8975d3c1538eb41923aa550de91
Lorenzo-1214 <rotunnolorenzo8@gmail.com>
2024-12-05 17:03:43 +0100

    iniziato css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,7 @@
 import './bootstrap';
 import 'bootstrap';
 import '../sass/app.scss';
+
+import './bootstrap';
+import 'bootstrap';
+import './script';
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - E-Commerce</title>
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</style>
+
 </head>
 <body>
     <!-- Header -->
